6db2ba46db7e9abfd40d5ca809148f75160269ec
Wadson Vaval <vavalson@hotmail.com>
2024-12-08 03:23:53 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export *  from './lib/logger';
 export * from  './schema/city.schema';
 export * from  './schema/user.schema';
+export * from  './schema/state.schema';
 export * from  './schema/country.schema';
 export * from  './middleware/validateParams';
