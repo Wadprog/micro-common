64c0713cce1a627fd54836372ac02710cc058676
Wadson Vaval <vavalson@hotmail.com>
2025-01-01 13:30:31 -0500

    update

--- src/sequelize/index.ts
+++ src/sequelize/index.ts
@@ -1,42 +1,18 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize, Options } from "sequelize";
 
-interface DbSettings {
-    host: string;
-    dialect: string;
-    database: string;
-    username: string;
-    password: string;
-}
 class SequelizeWrapper {
-    public _sequelize: undefined | Sequelize;
+    private _client: Sequelize | undefined = undefined;
 
-
-    constructor() { }
-    get sequelize() {
-
-        if (!this._sequelize)
-            throw new Error('Sequelize is not connected');
-        return this._sequelize;
+    connect(dbOptions: Options) {
+        this._client = new Sequelize(dbOptions)
     }
 
-    connect(dbSettings: DbSettings) {
-        // @ts-ignore
-        this._sequelize = new Sequelize({
-            logging: false,
-            ...dbSettings,
-            seederStorge: 'sequelize',
-        });
+    get client() {
+        if (!this._client)
+            throw new Error('Cannot get client before connecting')
+        return this._client
     }
-
-    startSequelize() {
-        const { models } = this.sequelize;
-        Object.keys(models).forEach((name) => {
-            if ('associate' in models[name]) {
-                (models[name] as any).associate(models);
-            }
-        });
-    }
-
 }
 
-export const sequelizeWrapper = new SequelizeWrapper();
+const sequelizeWrapper = new SequelizeWrapper()
+export default sequelizeWrapper;
